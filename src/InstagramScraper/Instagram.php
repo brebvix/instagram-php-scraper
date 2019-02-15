05f3a5463814f845220300462940f611e5b9e93c
Nazar <mebow.software@gmail.com>
2019-02-15 08:19:56 +0200

    Disable SSL checking when requesting (required for proxy)

--- src/InstagramScraper/Instagram.php
+++ src/InstagramScraper/Instagram.php
@@ -51,6 +51,16 @@ class Instagram
     private $userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36';
 
     /**
+     * Instagram constructor.
+     * Disable SSL verify for HTTP proxy
+     */
+    public function __construct()
+    {
+        Request::verifyPeer(false);
+        Request::verifyHost(false);
+    }
+
+    /**
      * @param string $username
      * @param string $password
      * @param null $sessionFolder
